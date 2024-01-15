d20493bf1b5abeb5b86338b5650ea10035f20069
Jorge Argudo <jorge_ruvi@hotmail.es>
2024-01-15 07:20:20 +0100

    Added environment variables

---
+++ src/app/environments/environment.development.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    INITIAL_HEROES: [
+        { id: 1, name: 'Superman' },
+        { id: 2, name: 'Batman' },
+        { id: 3, name: 'Batgirl' },
+        { id: 4, name: 'Manolito el fuerte' },
+        { id: 5, name: 'Spiderman' },
+        { id: 6, name: 'Wonderwoman' },
+        { id: 7, name: 'Hulk' },
+        { id: 8, name: 'Flash' }
+    ]
+};
