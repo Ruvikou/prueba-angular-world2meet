51652f993ea6f61f2291fca5715d3bdfb0564b15
Jorge Argudo <jorge_ruvi@hotmail.es>
2024-01-15 07:19:47 +0100

    Added background style and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { EditHeroComponent } from './edit-hero/edit-hero.component';
+import { NewHeroComponent } from './new-hero/new-hero.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
+    { path: 'edit/:id', component: EditHeroComponent },
+    { path: 'new', component: NewHeroComponent }
 ];
